30fcede316258c9fb2d04b76729310796d81aefa
Adam Getchell <acgetchell@ucdavis.edu>
2022-10-18 13:13:34 -0700

    More work on flip_n_to_m
    The function call is mostly there.

--- tests/flip_n_to_m_test.cpp
+++ tests/flip_n_to_m_test.cpp
@@ -5,16 +5,27 @@
 /// @date 2022-10-17
 
 // clang-format off
-#include <doctest/doctest.h>
 #include <CGAL/boost/bimap.hpp>
 #include <CGAL/Tetrahedral_remeshing/internal/flip_edges.h>
-#include <numbers>
+#include <CGAL/Tetrahedral_remeshing/internal/tetrahedral_adaptive_remeshing_impl.h>
 // clang-format on
+#include <CGAL/Tetrahedral_remeshing/Remeshing_triangulation_3.h>
+//#include <CGAL/tetrahedron_soup_to_triangulation_3.h>
+
+#include <doctest/doctest.h>
+
+#include <boost/optional/optional_io.hpp>
+#include <numbers>
 
 #include "bistellar_flip.hpp"
 
 static inline auto constexpr SQRT_2     = std::numbers::sqrt2_v<double>;
 static inline auto constexpr INV_SQRT_2 = 1.0 / SQRT_2;
+
+using CellsSmallVector = boost::container::small_vector<Cell_handle, 64>;
+using CellsSmallVectorOptional = boost::optional<CellsSmallVector>;
+using VertexIncidentCellsMap =
+    std::unordered_map<Vertex_handle, CellsSmallVectorOptional>;
 
 SCENARIO("Perform bistellar flip on Delaunay triangulation using CGAL " *
          doctest::test_suite("flip_n_to_m"))
@@ -32,12 +43,24 @@ SCENARIO("Perform bistellar flip on Delaunay triangulation using CGAL " *
     };
     Delaunay triangulation(points.begin(), points.end());
     CHECK(triangulation.is_valid());
+    WHEN("We convert to a remeshing triangulation")
+    {
+      using Remeshing_triangulation =
+          CGAL::Tetrahedral_remeshing::Remeshing_triangulation_3<
+              CGAL::Exact_predicates_inexact_constructions_kernel>;
+      //      Remeshing_triangulation triangulation_2 =
+      //      CGAL::tetrahedron_soup_to_triangulation_3<Remeshing_triangulation>(triangulation);
+      Remeshing_triangulation remeshing_triangulation;
+      //      remeshing_triangulation.tds(). = triangulation;
+    }
     WHEN("We find the pivot edge in the triangulation")
     {
       auto pivot_edge =
           find_pivot_edge(triangulation, get_finite_edges(triangulation));
       auto incident_cells =
           get_incident_cells(triangulation, pivot_edge.value());
+      auto                   vertices = get_vertices(incident_cells.value());
+      VertexIncidentCellsMap incident_cells_per_vertex;
       THEN("We have a pivot edge")
       {
         REQUIRE_MESSAGE(pivot_edge, "Pivot edge not found");
@@ -50,19 +73,35 @@ SCENARIO("Perform bistellar flip on Delaunay triangulation using CGAL " *
       }
       THEN("We can obtain the vertices from the cells incident to that edge")
       {
-        auto vertices = get_vertices(incident_cells.value());
         REQUIRE_EQ(vertices.size(), 6);
+      }
+      THEN(
+          "We can obtain the container of incident cells to each vertex participating in the bistellar flip")
+      {
+        for (auto const& vertex : get_vertices(incident_cells.value()))
+        {
+          CellsSmallVector incident_cells_b;
+          // Put the incident cells into incident_cells
+          triangulation.tds().incident_cells(
+              vertex, std::back_inserter(incident_cells_b));
+          // Put the result into an option type
+          CellsSmallVectorOptional incident_cells_opt(incident_cells_b);
+          // Put the result into a map
+          incident_cells_per_vertex.emplace(vertex, incident_cells_opt);
+        }
+        REQUIRE_EQ(incident_cells_per_vertex.size(), 6);
       }
       THEN("We can call flip_n_to_m")
       {
-        // Obtain top and bottom vertices by re-inserting, which returns the
-        // Vertex_handle
-        auto top = triangulation.insert(Point(0, 0, 2));
-        // auto visitor = ???
-
-        //        auto const result = CGAL::Tetrahedral_remeshing::internal::
-        //            flip_n_to_m(triangulation, pivot_edge.value(), top,
-        //            incident_cells, visitor);
+        //                auto const result =
+        //                CGAL::Tetrahedral_remeshing::internal::flip_n_to_m(
+        //                    pivot_edge, triangulation, vertices,
+        //                    CGAL::Tetrahedral_remeshing::internal::MIN_ANGLE_BASED,
+        //                    incident_cells_per_vertex,
+        //                    CGAL::Tetrahedral_remeshing::internal::Default_remeshing_visitor());
+        //                CHECK_EQ(
+        //                    result,
+        //                    CGAL::Tetrahedral_remeshing::Sliver_removal_result::VALID_FLIP);
       }
     }
   }
